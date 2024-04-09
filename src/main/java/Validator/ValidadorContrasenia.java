38f6639861f0692fa7f4c409e01d31b26a1dde35
Kevin Cazon <cazonleonel@gmail.com>
2024-04-09 20:46:45 -0300

    desarrollo de la clase ValidadorContrasenia
    re

--- src/main/java/Validator/ValidadorContrasenia.java
+++ src/main/java/Validator/ValidadorContrasenia.java
@@ -1,4 +1,27 @@
 package Validator;
 
+import errors.ValidationError;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class ValidadorContrasenia {
+
+  private List<Validator> validators = new ArrayList<>(
+      Arrays.asList(
+          new PeoresContrasenias(),
+          new LargoMinimo(),
+          new EspaciosEnBlanco()
+      )
+  );
+
+  public String Validate(String contrasenia) {
+    List<String> errors = new ArrayList<>();
+    errors=validators.stream().map(validator->validator.validate(contrasenia)).filter(Objects::nonNull).collect(Collectors.toList());
+    if (errors.size() > 0)
+      throw new ValidationError("Los errores fueron: \n - " + String.join("\n - ", errors));
+    return contrasenia;
+  }
 }
---
+++ src/main/java/errors/ValidationError.java
@@ -0,0 +1,8 @@
+package errors;
+
+public class ValidationError extends RuntimeException {
+  public ValidationError(String message) {
+    super(message);
+
+  }
+}
